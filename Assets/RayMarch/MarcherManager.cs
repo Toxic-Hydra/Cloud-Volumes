f2ffe49e10599089a369147faa696595560911ee
Ivan <ivanlopez3996@gmail.com>
2020-05-26 11:49:32 -0700

    initial commit, sdf volumes

---
+++ Assets/RayMarch/MarcherManager.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Since you can't just click and drag Compute shaders you gotta shove them into a script and start them through there.
+//It's why we define which function is the "Main" function within the compute script.
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class MarcherManager : MonoBehaviour
+{
+    public ComputeShader rayMarcher;
+    //Compute Shader tells us to do this.
+    RenderTexture target;
+
+    private Camera view;
+
+    private Light light;
+
+    private List<ComputeBuffer> buffers;
+
+    void Setup()
+    {
+        view = Camera.current;
+        light = FindObjectOfType<Light>();
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Setup();
+        buffers = new List<ComputeBuffer>();
+
+        InitRenderTexture();
+        CreateScene();
+        ParameterSetup();
+        
+        //Setting up the Raymarcher shader
+        rayMarcher.SetTexture(0, "Origin", src);
+        rayMarcher.SetTexture(0, "Result", target);
+
+        //Gotta be honest i'm not sure why the groups are calculated like this.
+        int threadGroupsX = Mathf.CeilToInt(view.pixelWidth / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(view.pixelHeight / 8.0f);
+        rayMarcher.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        Graphics.Blit(target, dest);
+
+        foreach (var buffer in buffers)
+        {
+            buffer.Dispose();
+        }
+    }
+
+    void InitRenderTexture()
+    {
+        if (target == null || target.width != view.pixelWidth || target.height != view.pixelHeight)
+        {
+            if(target != null)
+                target.Release();
+            
+            target = new RenderTexture(view.pixelWidth, view.pixelHeight, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            target.enableRandomWrite = true; //this is the important part
+            target.Create();
+        }
+    }
+
+    struct SphereData
+    {
+        public Vector3 position;
+        public Vector3 scale;
+
+        public int operation;
+        public float blending;
+        public int children;
+
+        public static int GetSize()
+        {
+            return sizeof(float) * 7 + sizeof(int) * 2;
+        }
+    }
+    void CreateScene()
+    {
+        List<Sphere> allSpheres = new List<Sphere>(FindObjectsOfType<Sphere>());
+        allSpheres.Sort((a,b) => a.operation.CompareTo(b.operation));
+        
+        List<Sphere> orderedSpheres = new List<Sphere>();
+
+        for (int i = 0; i < allSpheres.Count; i++)
+        {
+            //top level spheres
+            if (allSpheres[i].transform.parent == null)
+            {
+                Transform parentSphere = allSpheres[i].transform;
+                orderedSpheres.Add(allSpheres[i]);
+                allSpheres[i].children = parentSphere.childCount;
+                for (int j = 0; j < parentSphere.childCount; j++)
+                {
+                    if (parentSphere.GetChild(j).GetComponent<Sphere>())
+                    {
+                        orderedSpheres.Add(parentSphere.GetChild(j).GetComponent<Sphere>());
+                        orderedSpheres[orderedSpheres.Count - 1].children = 0;
+                    }
+                }
+            }
+        }
+        
+        SphereData[] sphereData = new SphereData[orderedSpheres.Count];
+        for (int i = 0; i < orderedSpheres.Count; i++)
+        {
+            var sphere = orderedSpheres[i];
+            sphereData[i] = new SphereData()
+            {
+                position = sphere.Position,
+                scale = sphere.Scale,
+                operation = (int) sphere.operation,
+                blending = sphere.blending * 3,
+                children = sphere.children
+            };
+        }
+        
+        int dataSize = sizeof(float) * 7 + sizeof(int) * 2;
+        ComputeBuffer sphereBuffer = new ComputeBuffer(sphereData.Length, dataSize);
+        sphereBuffer.SetData(sphereData);
+        rayMarcher.SetBuffer(0, "scene", sphereBuffer);
+        rayMarcher.SetInt("totalShapes", sphereData.Length);
+        
+        buffers.Add(sphereBuffer);
+    }
+
+    void ParameterSetup()
+    {
+        //ommitting light stuff
+        rayMarcher.SetMatrix("WorldCamera", view.cameraToWorldMatrix);
+        rayMarcher.SetMatrix("InverseCameraProj", view.projectionMatrix.inverse);
+        //Set light parameters.
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/RayMarch/Sphere.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+
+    public enum Operation
+    {
+        None,
+        Blend,
+        Cut,
+        Mask
+    }
+    public Operation operation;
+
+    [Range(0, 1)] public float blending;
+
+    [HideInInspector] public int children;
+
+    public Vector3 Position
+    {
+        get
+        {
+            return transform.position;
+        }
+    }
+
+    public Vector3 Scale
+    {
+        get
+        {
+            Vector3 pScale = Vector3.one;
+            if (transform.parent != null && transform.parent.GetComponent<Sphere>() != null)
+            {
+                pScale = transform.parent.GetComponent<Sphere>().Scale;
+            }
+
+            return Vector3.Scale(transform.localScale, pScale);
+        }
+    }
+}
